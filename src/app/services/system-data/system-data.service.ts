937cf91fffeabc82d1027826699ad18e5b1f9a27
Matthew Bjorkman <mbjorkman@bigtime.net>
2023-07-30 08:59:25 -0500

    Icon Improvements

--- src/app/services/system-data/system-data.service.ts
+++ src/app/services/system-data/system-data.service.ts
@@ -8,8 +8,11 @@ import { SysIcon } from 'src/app/shared/system-classes/system-objects';
 export class SystemDataService {
   initialized: boolean = false;
   icons!: SysIcon[];
+  iconsReduced!: SysIcon[];
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { 
+    this.loadIcons(() => {});
+  }
 
   private loadIcons(callback: (param: SysIcon[]) => void) {
     return this.http.get('./assets/system-data/icons.json')
@@ -17,16 +20,37 @@ export class SystemDataService {
             this.icons = result.icons;
             this.initialized = true;
             callback(this.icons);
+            this.createReducedList();
       })
   }
 
-  public getIcons(callback: (param: SysIcon[]) => void) {
+  private createReducedList() {
+    this.iconsReduced = this.icons.filter((icon) => {
+      if (icon.name.endsWith('-outline')) return false;
+      if (icon.name.endsWith('-sharp')) return false;
+      return true;
+    });
+  }
+
+  public getIcons(callback: (param: SysIcon[]) => void, getAll: boolean = false) {
       //give the service a chance to initialize
       if (!this.initialized) {
-        this.loadIcons(callback);
+        this.loadIcons(() => {
+          if (getAll) {
+            callback(this.icons);
+          }
+          else {
+            callback(this.iconsReduced);
+          }
+        });
       }
       else {
-        callback(this.icons);
+        if (getAll) {
+          callback(this.icons);
+        }
+        else {
+          callback(this.iconsReduced);
+        }
       }
   }
 }
