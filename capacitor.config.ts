9c03e4fffa5cdee7f725d7f7d265be2414c05c3e
Matthew Bjorkman <mbjorkman@bigtime.net>
2023-09-16 14:46:29 -0500

    With default config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -9,8 +9,10 @@ const config: CapacitorConfig = {
   },
   plugins: {
     LocalNotifications: {
-      iconColor: '#488AFF'
-    }
+      smallIcon: "ic_stat_icon_config_sample",
+      iconColor: "#488AFF",
+      sound: "beep.wav",
+    },
   }
 };
 
